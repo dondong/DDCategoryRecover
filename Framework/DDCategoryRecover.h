0da3fbe4e87952f7cfc1e1c3f523569a68a55953
dondong <dongmingdong@dongmingdongdeMacBook-Pro-2.local>
2022-09-27 00:57:08 +0800

    bugfix general

--- Framework/DDCategoryRecover.h
+++ Framework/DDCategoryRecover.h
@@ -6,11 +6,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface DDCategoryRecover : NSObject
 + (void)recoverCurrentMethodWithTarget:(id)target selector:(SEL)selector;
++ (void)recoverCurrentMethodWithTarget:(id)target selector:(SEL)selector recoverBlock:(void (^)(Method method))recoverBlock;
 @end
 
 NS_ASSUME_NONNULL_END
--- Framework/DDCategoryRecover.m
+++ Framework/DDCategoryRecover.m
@@ -8,37 +8,42 @@
 #import "DDCategoryRecover.h"
 #import <mach/mach.h>
 #import <dlfcn.h>
-#import <objc/runtime.h>
 
 @implementation DDCategoryRecover
 + (void)recoverCurrentMethodWithTarget:(id)target selector:(SEL)selector
 {
-    BOOL isStatics = NO;
+    void *pc = __builtin_return_address(0);
+    [self _recoverCurrentMethodWithTarget:target selector:selector pc:pc recoverBlock:^(Method method) {
+        void (*ptr)(id, SEL) = (void (*)(id, SEL))method_getImplementation(method);
+        ptr(target, selector);
+    }];
+}
+
++ (void)recoverCurrentMethodWithTarget:(id)target selector:(SEL)selector recoverBlock:(void (^)(Method method))recoverBlock {
+    void *pc = __builtin_return_address(0);
+    [self _recoverCurrentMethodWithTarget:target selector:selector pc:pc recoverBlock:recoverBlock];
+}
+
++ (void)_recoverCurrentMethodWithTarget:(id)target selector:(SEL)selector pc:(void *)pc recoverBlock:(void (^)(Method method))recoverBlock
+{
     Class c = [target class];
     if (c == target) {
         c = *(Class *)(__bridge void *)target;
-        isStatics = YES;
     }
-    void *pc = __builtin_return_address(0);
     Dl_info info;
     dladdr(pc, &info);
     do {
         Method m = NULL;
-        if (isStatics) {
-            m = class_getClassMethod(c, selector);
-        } else {
-            m = class_getInstanceMethod(c, selector);
-        }
-        IMP imp = method_getImplementation(m);
         int hit = 0;
         unsigned int count = 0;
         Method *list = class_copyMethodList(c, &count);
         for (int i = 0; i < count; ++i) {
-            if (method_getName(list[i]) == selector && (imp == info.dli_saddr || hit > 0)) {
+            if (method_getName(list[i]) == selector &&
+                (method_getImplementation(list[i]) == info.dli_saddr || hit > 0)) {
                 hit++;
+                IMP imp1 = method_getImplementation(list[i]);
                 if (hit > 1) {
-                    void (*ptr)(id, SEL) = (void (*)(id, SEL))method_getImplementation(list[i]);
-                    ptr(target, selector);
+                    recoverBlock(list[i]);
                 }
             }
         }
@@ -48,6 +53,5 @@
         }
         c = class_getSuperclass(c);
     } while(c != Nil);
-    
 }
 @end
